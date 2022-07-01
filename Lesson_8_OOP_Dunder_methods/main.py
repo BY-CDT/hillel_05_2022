972007ae0682baa304f3e22639d45bb0a629d762
BY-CDT <jampolskijbs@gmail.com>
2022-07-02 00:53:46 +0300

    Updated double conversion logic

--- Lesson_8_OOP_Dunder_methods/main.py
+++ Lesson_8_OOP_Dunder_methods/main.py
@@ -1,80 +1,75 @@
-currencies = [
-    {"from": "USD", "to": "UAH", "rate": 29.54},
-    {"from": "UAH", "to": "USD", "rate": 0.034},
-    {"from": "EUR", "to": "USD", "rate": 1.04},
-]
-
-
 class Price:
-    def __init__(self, price: int, currency: str) -> None:
-        self.price = price
+    def __init__(self, amount: int, currency: str) -> None:
+        self.price = amount
         self.currency = currency
 
-    def __add__(self, other) -> tuple:
-        if self.currency == other.currency:
-            result = self.price + other.price, self.currency
+    def __add__(self, other) -> float | str:
+        if not self.have_rate() or not other.have_rate():
+            return "No currency exchange rate available."
         else:
-            result = self.conversion() + other.conversion(), "UAH"
-        return result
-
-    def __sub__(self, other) -> float:
-        if self.currency == other.currency:
-            result = self.price - other.price, self.currency
-        else:
-            result = self.conversion() - other.conversion(), "UAH"
-        return result
-
-    def conversion(self):
-        if self.currency == "UAH":
-            result = self.price
-        elif self.currency == "USD":
-            result = self.conversion_2_uah()
-        else:
-            to_usd = Price(self.conversion_1_usd(), "USD")
-            result = to_usd.conversion_2_uah()
-        return result
-
-    def exchange_rate(self, currency_to):
-        rate = "No rate available."
-        for exchange_rate in currencies:
-            if all(
-                (
-                    exchange_rate.get("from") == self.currency,
-                    exchange_rate.get("to") == currency_to,
-                )
-            ):
-                rate = exchange_rate.get("rate")
-                break
+            if self.currency == other.currency:
+                return float(self.price + other.price)
             else:
-                continue
-        return rate
+                result = self.price + other.conversion(self.currency)
+                return round(result, 2)
 
-    def conversion_1_usd(self):
-        rate = self.exchange_rate("USD")
-        if isinstance(rate, str):
-            result = rate
+    def __sub__(self, other) -> float | str:
+        if not self.have_rate() or not other.have_rate():
+            return "No currency exchange rate available."
         else:
-            result = self.price * rate
-        return result
+            if self.currency == other.currency:
+                return float(self.price - other.price)
+            else:
+                result = self.price - other.conversion(self.currency)
+                return round(result, 2)
 
-    def conversion_2_uah(self):
-        rate = self.exchange_rate("UAH")
-        if isinstance(rate, str):
-            result = rate
+    def have_rate(self) -> bool:
+        if self.currency in currencies:
+            return True
         else:
-            result = self.price * rate
-        return result
+            return False
+
+    def get_rate(self, currency, from_usd=False):
+        if currency == "USD" and from_usd:
+            return 1
+        else:
+            for rate in rates:
+                if not from_usd:
+                    if rate.get("from_curr") == self.currency and rate.get("to_curr") == currency:
+                        return rate.get("rate")
+                else:
+                    if rate.get("from_curr") == "USD" and rate.get("to_curr") == currency:
+                        return rate.get("rate")
+
+    def conversion(self, end_currency):
+        if self.currency == "USD":
+            rate = self.get_rate(end_currency, from_usd=True)
+            return self.price * rate
+        else:
+            rate = self.get_rate("USD")
+            middle_price = self.price * rate
+            reverse_rate = self.get_rate(end_currency, from_usd=True)
+            return middle_price * reverse_rate
 
 
-product_a = Price(100, "UAH")
-product_b = Price(200, "USD")
-product_c = Price(100, "EUR")
+
+rates = [
+    {"from_curr": "UAH", "to_curr": "USD", "rate": 0.034},
+    {"from_curr": "UAH", "to_curr": "EUR", "rate": 0.032},
+    {"from_curr": "USD", "to_curr": "UAH", "rate": 29.54},
+    {"from_curr": "USD", "to_curr": "EUR", "rate": 0.96},
+    {"from_curr": "EUR", "to_curr": "USD", "rate": 1.04},
+    {"from_curr": "EUR", "to_curr": "UAH", "rate": 30.81}
+]
+
+currencies = set([entry.get("from_curr") for entry in rates])
+
+p1 = Price(100, "EUR")
+p2 = Price(200, "UAH")
 
 
 def main():
-    print(product_b + product_b)
-    exit()
-
+    print(p1 + p2)
 
 if __name__ == "__main__":
     main()
