23603532e9c720cda112d0e423630ea2179052b5
BY-CDT <jampolskijbs@gmail.com>
2022-07-09 11:12:22 +0300

    Added description to funcions in the async file.

--- Lesson_11_ Threads/async.py
+++ Lesson_11_ Threads/async.py
@@ -1,7 +1,8 @@
 import asyncio
 
 
-async def get_primes_amount(num: int):
+async def get_primes_amount(num: int) -> None:
+    """Checks how many prime numbers are there in count from 1 to the supplied argument number."""
     results = 0
     for item in range(1, num + 1):
         counter = 0
@@ -15,7 +16,9 @@ async def get_primes_amount(num: int):
     print(f"There are {results} prime numbers in {num}.")
 
 
-async def no_lock(num):
+async def no_lock(num: int) -> None:
+    """Calls asynchronously the get_primes_amount function. Allows multiple function calls to be executed
+    asynchronously."""
     await get_primes_amount(num)
 
 
@@ -23,6 +26,8 @@ numbers = [40000, 40, 5, 1, 40000, 700]
 
 
 def main():
+    """Creating an event loop for the asynchronous execution. Feeds the provided list of numbers to the functions in
+    order to get the count of prime numbers for each number provided from the list."""
     task = [no_lock(num) for num in sorted(numbers)]
     event_loop = asyncio.get_event_loop()
     event_loop.run_until_complete(asyncio.gather(*task))
